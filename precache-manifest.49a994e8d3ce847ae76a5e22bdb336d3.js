dac02c786e90171fc657be0bfdf6afd587eaf31e
Dipendra Pathak <subaspathak62@gmail.com>
2019-11-27 15:35:57 +0545

    Updates

---
+++ precache-manifest.49a994e8d3ce847ae76a5e22bdb336d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf78ed4771c7ad9909e8b90e62210a2",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d4148a86bc3b2b49c74f",
+    "url": "/react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6a41b57dbea7012d6338",
+    "url": "/react-app/static/js/2.6067a914.chunk.js"
+  },
+  {
+    "revision": "d4148a86bc3b2b49c74f",
+    "url": "/react-app/static/js/main.6cd2d767.chunk.js"
+  },
+  {
+    "revision": "fa8a5ac7b64fef79a8ac",
+    "url": "/react-app/static/js/runtime-main.4013ad70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app/static/media/logo.25bf045c.svg"
+  }
+]);
